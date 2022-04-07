2f042ff2dc7bef7e47ef51df46547cf882a82822
EhsanRD <ehsan.rezaei.d@email.com>
2022-04-07 13:16:55 +1000

    convert num1 to num2

--- calc.py
+++ calc.py
@@ -1,16 +1,16 @@
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
-    num0, operator, num1 = expression.split(' ')
-    num0, num1 = float(num0), float(num1)
+    num0, operator, num2 = expression.split(' ')
+    num0, num2 = float(num0), float(num2)
     if operator == '+':
-        return num0 + num1
+        return num0 + num2
     elif operator == '-':
-        return num0 - num1
+        return num0 - num2
     elif operator == '*':
-        return num0 * num1
+        return num0 * num2
     elif operator == '/':
-        return num0 / num1
+        return num0 / num2
     else:
         print('unknown operator!')
         return None
