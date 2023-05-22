b8424fc190631acd490169373617b2cb93aab5ad
Ranjeet Singh <ranjeet.singh2@monash.edu>
2023-05-22 12:04:28 +1000

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,9 +1,8 @@
-#comment
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
-    num0, num1 = float(num0), float(num1)
+    num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
@@ -13,7 +12,5 @@ def compute(expression):
     elif operator == '/':
         return num0 / num1
     else:
-        print('unknown operator! Only +-/* available.')
+        print('unknown operator!')
         return None
-
-#this is a comment
